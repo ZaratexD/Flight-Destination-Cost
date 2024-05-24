31d8d54c88450de27bec0e7bb0e8edfcfc983644
Angel Zarate <zarate@uw.edu>
2024-05-23 18:04:02 -0700

    rand.py exports html.txt

--- data/rand.py
+++ data/rand.py
@@ -1,15 +1,19 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.service import Service
 from webdriver_manager.firefox import GeckoDriverManager
+from bs4 import BeautifulSoup
 import time
 
 # Setup Selenium WebDriver using webdriver_manager
 options = webdriver.FirefoxOptions()
 options.add_argument('--headless')  # Run in headless mode for faster execution
-executable_path = '/usr/local/bin/geckodriver'
+
+# Setup the GeckoDriver using Service
+service = Service(GeckoDriverManager().install())
 
 # Initialize the WebDriver
-driver = webdriver.Firefox(executable_path=executable_path, options=options)
+driver = webdriver.Firefox(service=service, options=options)
 
 # URL of the flight schedule page
 url = 'https://www.delta.com/flightstatus/schedule/SEA/FCO/2024-06-03'
@@ -23,8 +27,15 @@ try:
     table = driver.find_element(By.CSS_SELECTOR, '#maincontent > dl-flightschedule > div > div > div > div.row.main-panel > div.col-xl-9.col-lg-9.col-md-12.col-12.right-panel-holder.ng-star-inserted > div > dl-flightroutes > table')
     table_html = table.get_attribute('outerHTML')
     
-    # Print the extracted table HTML
-    print(table_html)
+    # Use BeautifulSoup to pretty-print the HTML
+    soup = BeautifulSoup(table_html, 'lxml')
+    pretty_table_html = soup.prettify()
+    
+    # Write the prettified HTML to a file
+    with open("schedule.txt", "w", encoding="utf-8") as file:
+        file.write(pretty_table_html)
+
+    print("The schedule has been written to schedule.txt")
 
 except Exception as e:
     print(f"An error occurred: {e}")
